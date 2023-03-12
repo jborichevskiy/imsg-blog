713f51918496a450592efc052d59773a58a5a931
jborichevskiy <jborichevskiy@gmail.com>
2023-03-12 03:50:20 -0600

    replace w env vars

--- pages/api/data.js
+++ pages/api/data.js
@@ -1,9 +1,8 @@
 import { createClient } from "@supabase/supabase-js";
 
 const supabase = createClient(
-  "https://fzuptiqapwizenjjpwca.supabase.co",
-  // SECRET KEY
-  "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImZ6dXB0aXFhcHdpemVuampwd2NhIiwicm9sZSI6InNlcnZpY2Vfcm9sZSIsImlhdCI6MTY3MTAwMzgzMSwiZXhwIjoxOTg2NTc5ODMxfQ.KOguLN8awgi23zXPmSUyKTGs7_y2RKUr6Cbt9f_eqjU"
+  process.env.NEXT_PUBLIC_PCI_PHOTOS_SUPABSE_URL,
+  process.env.PCI_PHOTOS_SUPABASE_SECRET_KEY
 );
 
 export default async function handler(req, res) {
