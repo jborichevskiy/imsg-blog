f4da20377dea8ab554be58ecc3db6768c5a209b2
jborichevskiy <jborichevskiy@gmail.com>
2023-03-12 03:55:11 -0600

    make absolute url

--- pages/feed/[id].js
+++ pages/feed/[id].js
@@ -81,7 +81,7 @@ export async function getServerSideProps(context) {
   const id  = query.id;
   console.log({ id });
 
-  const url = `/api/data?author=${id}`;
+  const url = `https://imsg-blog.vercel.app/api/data?author=${id}`;
 
   const data = await fetch(url);
   const dataJSON = await data.json();
